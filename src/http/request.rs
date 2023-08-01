6aea7dfb05149ddaf1835de5c13d4ed159a15b65
Mark Hughes <wethestrongapparel@gmail.com>
2023-08-01 00:40:38 -0400

    Updated some error handling methods.

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,8 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Result as FtmResult, Display, Formatter, Debug};
+
 
 pub struct Request {
     path: String,
@@ -7,12 +10,40 @@ pub struct Request {
     method: Method,
 }
 
-impl Request {
-     fn from_byte_array(buf: &[u8]) -> Result<Self, String> {}
-}
 
 impl TryFrom<&[u8]> for Request { // Byte conversion error handling.
-    type Error = String;
+    type Error = ParseError;
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error>{unimplemented!()}
 }
 
+
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl Error for ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+// FMT display error
+impl Dispay for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FtmResult{
+        write!(f, "{}" , self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FtmResult{
+        write!(f, "{}" , self.message())
+    }
+}
--- src/server.rs
+++ src/server.rs
@@ -5,35 +5,35 @@ use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
-        addr: String,
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
     }
+    pub fn run(self) {
+        println!("Listening on {:?}", self.addr);
 
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self { addr }
-        }
-        pub fn run(self) {
-            println!("Listening on {:?}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
 
-            let listener = TcpListener::bind(&self.addr).unwrap();
-
-            loop {
-                match listener.accept() {
-                    Ok((mut stream, _)) => {
-                        let mut buffer = [0; 1024]; // BUFFER SIZE MAY BE TOO SMALL FOR PRODUCTION READINESS.
-                        match stream.read(&mut buffer) {
-                            Ok(_) => {
-                                println!("Connection Request: {}", String::from_utf8_lossy(&buffer)); // Converts bytes to strings even if invalid chars.
-                                match Request::try_from(&buffer[..]) {
-                                    Ok(request) => {},
-                                    Err(e) => println!("Failed to parse request: {}", e)
-                                }
-                            },
-                            Err(e) => println!("There was a connection issue:{}", e)
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024]; // BUFFER SIZE MAY BE TOO SMALL FOR PRODUCTION READINESS.
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Connection Request: {}", String::from_utf8_lossy(&buffer)); // Converts bytes to strings even if invalid chars.
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse request: {}", e),
+                            }
                         }
+                        Err(e) => println!("There was a connection issue:{}", e),
                     }
-                    Err(e) => println!("Failed to establish a connection. {}", e)
                 }
+                Err(e) => println!("Failed to establish a connection. {}", e),
             }
         }
     }
+}
